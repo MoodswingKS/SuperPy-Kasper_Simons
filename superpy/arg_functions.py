c2ae1fbc35948d04f6e8e2e480ec3cbca9238584
MoodswingKS <47191523+MoodswingKS@users.noreply.github.com>
2021-09-22 12:39:05 +0200

    02 added buysell class, format dmy, getID, more parsers

--- superpy/arg_functions.py
+++ superpy/arg_functions.py
@@ -1,12 +1,9 @@
 import os
 import csv
+import argparse
+from datetime import datetime
 
-class Products():
-    def __init__(self, data_paths):
-        dir_path = os.getcwd()
-        data_paths = f'{dir_path}/data/'
-        self.data_paths = data_paths
-
+this_date = datetime.now().date()
 
 class Inventory():
     def __init__(self):
@@ -24,7 +21,7 @@ class Inventory():
                     for row in reader:
                         print(row)
 
-
+# both next classes need to do a check on inventory before adding their items to find a possible id
 class Bought():
     def __init__(self):
         dir_path = os.getcwd()
@@ -32,26 +29,78 @@ class Bought():
         self.bought_path = os.path.join(data_paths, 'bought.csv')
     
     def buy(self, product_name, amount, buy_price, exp_date):
-        # check inventory
-        # if os.path.isfile(self.bought_path):
-        #     with open(self.bought_path, newline='') as csv_file:
-        #         reader = csv.reader(csv_file)
-        #         next(reader, None)
-        #         for line in reader:
-        #             if product_name != line['product_name']:
-        #                 product_id = len(line) + 1
         if os.path.isfile(self.bought_path):
             with open(self.bought_path, 'w', newline='') as csv_file:
-                count = 100
+                id_product = getID(product_name)
                 writer = csv.writer(csv_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
-                id_product = count + 1
                 writer.writerow([id_product, product_name, amount, buy_price, exp_date])
             csv_file.close()
+    # i should change inventory csv based on buying
 
         
 
 class Sold():
+    def __init__(self):
+        dir_path = os.getcwd()
+        data_paths = f'{dir_path}/data/'
+        self.sold_path = os.path.join(data_paths, 'sell.csv')
+    
+    def sell(self, product_name, amount, sell_price, exp_date):
+        if os.path.isfile(self.sold_path):
+            with open(self.sold_path, 'w', newline='') as csv_file:
+                writer = csv.writer(csv_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+                id_product = getID(product_name)
+                writer.writerow([id_product, product_name, amount, sell_price, exp_date])
+            csv_file.close()
+    # I should change inventory csv after selling an item
+
+def getID(product_name):
+    dir_path = os.getcwd()
+    data_paths = f'{dir_path}/data/'
+    inventory_path = os.path.join(data_paths, 'inventory.csv')
+    
+    if os.path.isfile(inventory_path):
+        with open(inventory_path, newline='') as csv_file:
+            reader = csv.reader(csv_file)
+            list_reader = list(reader)
+            for row in list_reader:
+                if product_name == row[1]:
+                    return print(row[0])
+            else:
+                return len(list_reader) + 1
+
+def remove_products(day):
+    dir_path = os.getcwd()
+    data_paths = f'{dir_path}/data/'
+    inventory_path = os.path.join(data_paths, 'inventory.csv')
+    remove_list = []
+
+    if os.path.isfile(inventory_path):
+        with open(inventory_path, newline='') as csv_file:    
+            reader = csv.reader(csv_file)
+            list_reader = list(reader)
+            for row in list_reader:
+                if day == row[3]:
+                    remove_list.append(row)
+        # with open(inventory_path, 'w', newline='') as csv_file:
+        #         writer = csv.writer(csv_file, delimiter = ',', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+        #         writer.
+        #     csv_file.close()
+    
+
+def valid_date(s):
+    try:
+        return datetime.strptime(s, "%d-%m-%y")
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+def timeLapse():
+    # include function that buys stuff with time 
+    # find product based on id in inventory, and buy something or / and sell something by random?
+    # change the csv on variables 
     pass
+
 
 super_inventory = Inventory()
 super_bought = Bought()
--- superpy/main.py
+++ superpy/main.py
@@ -1,7 +1,9 @@
 # Imports
 import argparse
+from datetime import datetime
 # from datetime import date
-from arg_functions import super_inventory, super_bought, super_sold
+from arg_functions import super_inventory, super_bought, super_sold, valid_date
+from parser_text import credits
 from create_csv import create
 
 
@@ -10,6 +12,11 @@ __winc_id__ = 'a2bc36ea784242e4989deb157d527ba0'
 __human_name__ = 'superpy'
 
 
+this_date = datetime.now().date()
+
+# still need to make the date information
+# can create a remove expired products with date information
+# change help screen with the parsers
 def main():
     # global parser init
     parser = argparse.ArgumentParser(description="SUPERPY")
@@ -19,23 +26,36 @@ def main():
     
     # buy parser
     buy_parser = mainparsers.add_parser('buy', help="Buy products")
-    buy_parser.add_argument('--product', type=str, help='product name', required=True)
-    buy_parser.add_argument('--amount', type=int, help='product amount', default=1)
-    buy_parser.add_argument('--buy_price', type=float, help='buy price per product', required=True)
-    buy_parser.add_argument('--exp_date', type=str, help='expiration date of product', required=True)
+    buy_parser.add_argument('-p', '--product', type=str, help='product name', required=True)
+    buy_parser.add_argument('-a', '--amount', type=int, help='product amount', default=1)
+    buy_parser.add_argument('-b', '--buy_price', type=float, help='buy price per product', required=True)
+    buy_parser.add_argument('-e', '--exp_date', type=valid_date, help='expiration date of product - format dd/mm/yyyy', required=True)
     # dest='exp_date'
     # optional date of buying
 
     # sell parser
     sell_parser = mainparsers.add_parser('sell', help="Sell products")
-    sell_parser.add_argument('--product', type=str, help='product name', required=True)
-    sell_parser.add_argument('--amount', type=int, help='product amount', default=1)
-    sell_parser.add_argument('--sell_price', type=float, help='sell price per product', required=True)
+    sell_parser.add_argument('-p', '--product', type=str, help='product name', required=True)
+    sell_parser.add_argument('-a', '--amount', type=int, help='product amount', default=1)
+    sell_parser.add_argument('-s', '--sell_price', type=float, help='sell price per product', required=True)
+    sell_parser.add_argument('-e', '--exp_date', type=valid_date, help='expiration date - format dd/mm/yyyy', required=True)
     # might need date of selling
     
     # inventory parser
     inv_parser = mainparsers.add_parser('inventory', help="Get a list of products")
     inv_parser.add_argument('show', help='Get a list of products')
+
+    # advance time parser
+    adv_parser = mainparsers.add_parser('time', help="Tell me my profit over a certain period of time")
+    adv_parser.add_argument('-t', '--time', help="amount of days", type=int, required=True)
+
+    # remove expired products parser
+    remove_parser = mainparsers.add_parser('remove-product', help="Remove expired products from list")
+    remove_parser.add_argument('-t', '--time', type=str, help="remove products", default=this_date, required=True)
+
+    # credits parser
+    credit_parser = mainparsers.add_parser('credits', help="CREDITS")
+    credit_parser.add_argument('super', help="Show me who made this!")
 
     args = parser.parse_args()
     return choose_args(args)
@@ -47,7 +67,13 @@ def choose_args(args):
     elif args.options == 'buy':
         super_bought.buy(args.product, args.amount, args.buy_price, args.exp_date)
     elif args.options == 'sell':
-        print('sell')
+        super_sold.sell(args.product, args.amount, args.sell_price, args.exp_date)
+    elif args.options == 'remove-product':
+        pass
+    elif args.options == 'time':
+        pass    
+    elif args.options == 'credits':
+        credits()
 
 
 
